ceb2eff06b9df2f7f0dd164c36657154ec8ea219
hauntarl <sameer.mungole@gmail.com>
2024-04-19 20:39:10 -0500

    Write stub implementation for all api endpoints

--- Todo/Controllers/TasksController.cs
+++ Todo/Controllers/TasksController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Todo.Contracts.Task;
 
 namespace Todo.Controllers;
 
@@ -15,8 +16,45 @@ public class TasksController : ControllerBase
 
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult GetTasks()
+    public IActionResult FetchTasks(
+        [FromQuery] bool? completed,
+        [FromQuery(Name = "sort_by")] string? sortBy)
     {
-        return Ok("Tasks");
+        return Ok();
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult FetchTask(Guid id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult CreateTask(CreateTaskRequest request)
+    {
+        return CreatedAtAction(
+            actionName: nameof(FetchTask),
+            routeValues: new { id = Guid.NewGuid() },
+            value: request);
+    }
+
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdateTask(Guid id, UpdateTaskRequest request)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return NoContent();
     }
 }
